68dabc604ef0d65929971178ca1d20811756656e
rowstu <51785801+rowstu@users.noreply.github.com>
2025-11-03 18:46:55 +0000

    Create uom.sh

---
+++ uom.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+pacman -S nmap openssh sshfs gparted rdesktop python wget screen ncdu gnu-netcat
+
+pacman -S exfat-utils ntfs-3g intel-ucode p7zip rsync unzip ddrescue testdisk gvfs-smb gvfs-nfs mlocate dosfstools tk minicom clamav
+
+pacman -S libmtp mtpfs ffmpeg dmidecode atomicparsley 
+
+pacman -S  vlc remmina freerdp xarchiver gvfs-mtp gimp meld 
+
+pacman -S  firefox chromium epiphany links elinks lynx w3m
+
+pacman -S  terminator xterm bash-completion vim
+
+pacman -S  libreoffice-fresh-en-gb evince
+
+pacman -S  xed htop filezilla thunderbird mutt newsboat
+
+pacman -S  xorg-server xorg-apps xorg-xinit xorg-xauth archlinux-wallpaper
+
+pacman -S xfce4 xfce4-goodies alacarte libcanberra pavucontrol
+
+pacman -S  nemo-share nemo-terminal nemo-python nemo-fileroller
+
+pacman -S  ttf-bitstream-vera ttf-liberation ttf-droid ttf-dejavu
+
+pacman -S  networkmanager-vpnc networkmanager-pptp net-tools vpnc
+
+pacman -S  flameshot
+
+pacman -S  cups cups-pdf hplip
+
+pacman -S  chrony
+
+pacman -S  dconf-editor
+
+pacman -S  usbutils
+
+pacman -S  openvpn networkmanager-openvpn
+
+pacman -S man-db man-pages
+
+pacman -S keepassxc yt-dlp tigervnc jq neofetch
+
+pacman -S gnome-keyring
+
+pacman -S network-manager-applet
+
+echo "optional install xf86-video-intel vscodium-bin proton-bridge signal-desktop-beta-bin github-desktop-bin clipit brave-bin google-chrome virtualbox-ext-oracle spotify-adblock zotify fslint-gui"
+
+
+echo "add exec cinnamon-session to .xinitrc"
